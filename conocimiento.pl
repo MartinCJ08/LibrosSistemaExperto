5825e3b33f203109fac6f314e1557f61962d9268
MartinCJ08 <martin.carrascoj@gmail.com>
2019-05-08 00:00:20 -0600

    Estructura general del proyecto

---
+++ conocimiento.pl
@@ -0,0 +1,22 @@
+conocimiento('un_mundo_feliz',
+	['Me gustan los libros que se desarrollan en un mundo distopico','Quiero leer un libro enfocado en el futuro']).
+
+conocimiento('1984',
+	['Me gustan los libros que se desarrollan en un mundo distopico']).
+
+conocimiento('el_resplandor',
+	['Me gustan los libros de terror','Prefiero un libro extenso']).
+
+conocimiento('orgullo_y_prejuicio',
+	['Me gustan los libros de amor']).
+ 
+conocimiento('moby_dick',
+	['Me gustan los libros de aventura','Prefiero un libro de pocas hojas']).
+
+id_imagen_preg('Me gustan los libros de amor','lupa').
+id_imagen_preg('Me gustan los libros de terror','lupa').
+id_imagen_preg('Me gustan los libros de aventura','lupa').
+id_imagen_preg('Me gustan los libros que se desarrollan en un mundo distopico','lupa').
+id_imagen_preg('Prefiero un libro de pocas hojas','lupa').
+id_imagen_preg('Prefiero un libro extenso','lupa').
+id_imagen_preg('Quiero leer un libro enfocado en el futuro','lupa').
